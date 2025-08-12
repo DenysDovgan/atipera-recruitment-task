7e7cb66034a2a1fd3156d352bae49e0405f6d26a
Denys Dovgan <131907373+DenysDovgan@users.noreply.github.com>
2025-08-12 20:09:12 +0200

    Implemented GithubService logic for parsing fetched github repositories data and map them into custom classes

--- src/main/java/com/example/atiperarecruitmenttask/service/GithubService.java
+++ src/main/java/com/example/atiperarecruitmenttask/service/GithubService.java
@@ -1,26 +1,46 @@
 package com.example.atiperarecruitmenttask.service;
 
 import com.example.atiperarecruitmenttask.client.GithubClient;
+import com.example.atiperarecruitmenttask.model.dto.GithubBranchDto;
 import com.example.atiperarecruitmenttask.model.dto.GithubRepositoryDto;
+import com.example.atiperarecruitmenttask.model.dto.githubclient.GithubBranchResponse;
 import com.example.atiperarecruitmenttask.model.dto.githubclient.GithubRepositoryResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GithubService {
     private final GithubClient githubClient;
 
     public List<GithubRepositoryDto> getNonForkRepositories(String username) {
-        /*List<GithubRepositoryDto> outputRepos = new ArrayList<>();
+        log.debug("Listing non-fork repositories for user: {}", username);
+
         List<GithubRepositoryResponse> fetchedRepos = githubClient.getUserRepos(username);
 
-        for (GithubRepositoryResponse repo : fetchedRepos) {
+        return fetchedRepos.stream()
+                .filter(r -> !r.fork())
+                .map(r -> new GithubRepositoryDto(
+                        r.name(),
+                        r.owner().login(),
+                        mapBranches(r.owner().login(), r.name())
+                ))
+                .toList();
+    }
 
-        }*/
+    private List<GithubBranchDto> mapBranches(String owner, String repoName) {
+        List<GithubBranchResponse> fetchedBranches = githubClient.getRepoBranches(owner, repoName);
 
+        return fetchedBranches.stream()
+                .map(b -> new GithubBranchDto(
+                        b.name(),
+                        b.commit() != null ? b.commit().sha() : null
+                ))
+                .toList();
     }
 }
